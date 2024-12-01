d5baf4caead78959177cce7480fcc4551269af4f
ElG87 <115730576+ElG87@users.noreply.github.com>
2024-12-01 09:55:45 +0100

    Update

--- js/motivatie.js
+++ js/motivatie.js
@@ -1,5 +1,6 @@
 const quotes = require('inspirational-quotes');
 
+/*
 console.log(Quote.getQuote()); // returns quote (text and author)
 console.log(Quote.getQuote({ author: false })); // return quote without author
 console.log(Quote.getRandomQuote()); // return any random quote
@@ -8,3 +9,4 @@ const randomQuote = quotes.getRandomQuote();
 console.log(randomQuote);
 
 document.getElementById('quote').innerText = randomQuote.text;
+*/
