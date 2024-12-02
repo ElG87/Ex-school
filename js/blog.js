c7599eb074e44a256682c6438e13aa7b4189a6ad
ElG87 <115730576+ElG87@users.noreply.github.com>
2024-12-03 00:33:25 +0100

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -12,10 +12,15 @@ $(document).ready(function () {
             //Alleen de eerste 3 artikelen tonen
             artikelen.slice(0,3).each(function (){
             
+            //Haalt de bron van de afbeelding op
+            const imgSrc = $(this).find('img').attr('src')
+
                 // Voeg elk artikel toe aan het overzicht
                 $('#blog').append(`
                     <div class="artikel">
+                    ${imgSrc ?`<img src="${imgSrc}' alt="Artikel afbeelding">` :''}
                 <h2>${$(this).find('h2').text()}</h2>
+                
                 <p>${$(this).find('p').text()}</p>
                 <a href="detailpagina.html">Lees meer</a>
                 </div>
