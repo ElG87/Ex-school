94ee47d1c58b9f8b955c580ce6ae1d1891bb2c86
ElG87 <115730576+ElG87@users.noreply.github.com>
2024-12-02 22:10:50 +0100

    update

--- js/motivatie.js
+++ js/motivatie.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('main'));
+const root = ReactDOM.createRoot(document.getElementById('quote'));
 root.render(
   <React.StrictMode>
     <App />
