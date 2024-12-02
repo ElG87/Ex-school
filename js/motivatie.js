4da151e06006309e265ad00a9fbce854d7c0752c
ElG87 <115730576+ElG87@users.noreply.github.com>
2024-12-02 22:08:09 +0100

    update

--- js/motivatie.js
+++ js/motivatie.js
@@ -1,3 +1,15 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('main'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 // const quotes = require('inspirational-quotes');
 
 // const randomQuote = quotes.getRandomQuote();
