6da6ad4f6b01ff64c8b48606756c8f6cfb059244
ElG87 <115730576+ElG87@users.noreply.github.com>
2025-04-06 22:35:01 +0200

    updates

---
+++ js/ui-effecten.js
@@ -0,0 +1,45 @@
+
+$( function() {
+    function runEffect() {
+      let selectedEffect = $( "#effectTypes" ).val() || "scale";
+ 
+      let options = {};
+        $( "#button" ).on( "click", function() {
+
+            $( "#effect" ).toggle( selectedEffect, options, 500, function() {
+            if ( selectedEffect === "scale" ) {
+                options = { percent: 100 };
+                } else if ( selectedEffect === "size" ) {
+                 options = { to: { width: 200, height: 60 } };
+            };
+                    $( "#effect" ).animate({
+                        backgroundColor: "#f5720c",
+                        color: "#fff",
+                        width: 300
+                        }, 1000, function() {
+
+                    $( "#effect" ).animate({ 
+                        backgroundColor: "#ffb489",
+                        color: "#000000",
+                        width: 480
+                        }, 2000 );
+                        });
+                });
+                
+        
+        }); 
+    };
+    runEffect();
+});
+  
+
+
+/*
+$( function() { //Div veranderd van kleur en grootte
+    let state = true;
+    $( "#button" ).on( "click", function() {
+      
+      state = !state;
+    });
+  } );
+*/
