6cd9a57dc267c2956eee26f58349f082814d2164
ElG87 <115730576+ElG87@users.noreply.github.com>
2025-04-08 19:19:53 +0200

    Update ui-effecten.js

--- js/ui-effecten.js
+++ js/ui-effecten.js
@@ -2,29 +2,29 @@
 $( function() {
     function runEffect() {
       let selectedEffect = $( "#effectTypes" ).val() || "scale";
- 
       let options = {};
+        
         $( "#button" ).on( "click", function() {
 
-            $("#effect").slideToggle(500, function() {
+            $("#effect").slideToggle(500, function() { // slideToggle animatie
             if ( selectedEffect === "scale" ) {
                 options = { percent: 100 };
                 } else if ( selectedEffect === "size" ) {
                  options = { to: { width: 200, height: 60 } };
             };
-                    $( "#effect" ).animate({
-                        backgroundColor: "#f5720c",
-                        color: "#fff",
-                        width: 300
-                        }, 1000, function() {
+         $( "#effect" ).animate({ // achtergrondkleur en tekstkleur veranderen
+            backgroundColor: "#f5720c",
+            color: "#fff",
+            width: 300
+            }, 1000, function() {
 
-                    $( "#effect" ).animate({ 
-                        backgroundColor: "#ffb489",
-                        color: "#000000",
-                        width: 480
-                        }, 2000 );
-                        });
-                });
+        $( "#effect" ).animate({ //verandering van de achtergrondkleur en tekstkleur na de andere animatie
+            backgroundColor: "#ffb489",
+            color: "#000000",
+            width: 480
+            }, 2000 );
+            });
+    });
         }); 
     };
     runEffect();
